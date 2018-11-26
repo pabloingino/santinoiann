4010d53dc88f74d5db3167a7270a09e1f8063d44
pingino <pingino@mininterior.gob.ar>
2018-11-26 15:29:01 -0300

    se eliminaron los archivos

--- admin/funciones/funciones.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-    require_once('../includes/funciones/bd_conexion.php');
-    require_once('../includes/funciones/funciones.php');
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php include_once 'includes/templates/header.php'; ?>
-  <section class="seccion contenedor">
-  </section>  
-  <?php include_once 'includes/templates/footer.php'; ?>
